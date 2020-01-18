6c6b7846379df4fe9b04f10a05335d177b40bbf9
Phil Nadon <phil@nadon.io>
2020-01-18 14:51:14 -0700

    reformatted files

--- src/main.js
+++ src/main.js
@@ -8,14 +8,15 @@ Author: Philippe Nadon
 
 */
 
-const { app, BrowserWindow, Menu} = require('electron');
+const { app, BrowserWindow, Menu } = require("electron");
 const ipcMain = require("electron").ipcMain;
-const path = require('path');
+const path = require("path");
 
 Menu.setApplicationMenu(false);
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
   app.quit();
 }
 
@@ -34,13 +35,13 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
+  mainWindow.on("closed", () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
@@ -51,18 +52,18 @@ const createWindow = () => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', () => {
+app.on("window-all-closed", () => {
   // On OS X it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
+  if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
@@ -71,8 +72,8 @@ app.on('activate', () => {
 });
 
 ipcMain.on("load-page", () => {
-  mainWindow.loadFile(path.join(__dirname, 'index.html'));
-})
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
